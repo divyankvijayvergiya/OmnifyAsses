cc4c5d4e830d2bb48f45d30816eb64d4ec8ee229
Divyank Vijayvergiya <divyankvijay.96@gmail.com>
2018-01-23 13:21:05 +0530

    merge sort implemented

--- app/src/main/java/hire/omnify/com/omnifyasses/SortServices.java
+++ app/src/main/java/hire/omnify/com/omnifyasses/SortServices.java
@@ -39,6 +39,7 @@ public class SortServices extends Service {
 
         Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
         quickSortFun();
+        mergeSortFun();
 
         return START_STICKY;
     }
@@ -111,6 +112,83 @@ public class SortServices extends Service {
         }
 
         return list;
+    }
+
+    public void mergeSortFun() {
+        arrayList = new ArrayList<>();
+
+
+        for (int i = 0; i < 20; ) {
+            Random random = new Random();
+            int min = 10;
+            int max = 98;
+            int rand = random.nextInt((max - min) + 1) + min;
+            if (!arrayList.contains(rand)) {
+                arrayList.add(rand);
+                i++;
+                mergeSort(arrayList);
+            }
+        }
+
+
+
+
+    }
+
+    public ArrayList<Integer> mergeSort(ArrayList<Integer> whole) {
+        ArrayList<Integer> left = new ArrayList<Integer>();
+        ArrayList<Integer> right = new ArrayList<Integer>();
+        int center;
+
+        if (whole.size() == 1) {
+            return whole;
+        } else {
+            center = whole.size()/2;
+            for (int i=0; i<center; i++) {
+                left.add(whole.get(i));
+            }
+
+            for (int i=center; i<whole.size(); i++) {
+                right.add(whole.get(i));
+            }
+
+            left  = mergeSort(left);
+            right = mergeSort(right);
+
+            merge(left, right, whole);
+        }
+        return whole;
+    }
+
+    private void merge(ArrayList<Integer> left, ArrayList<Integer> right, ArrayList<Integer> whole) {
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int wholeIndex = 0;
+        while (leftIndex < left.size() && rightIndex < right.size()) {
+            if ( (left.get(leftIndex).compareTo(right.get(rightIndex))) < 0) {
+                whole.set(wholeIndex, left.get(leftIndex));
+                leftIndex++;
+            } else {
+                whole.set(wholeIndex, right.get(rightIndex));
+                rightIndex++;
+            }
+            wholeIndex++;
+        }
+
+        ArrayList<Integer> rest;
+        int restIndex;
+        if (leftIndex >= left.size()) {
+            rest = right;
+            restIndex = rightIndex;
+        } else {
+            rest = left;
+            restIndex = leftIndex;
+        }
+
+        for (int i=restIndex; i<rest.size(); i++) {
+            whole.set(wholeIndex, rest.get(i));
+            wholeIndex++;
+        }
     }
 
 
